1b183b0ac503f688459214a7236406d010bceb2f
Ferran Ramírez <ferran@berebel.io>
2022-08-09 10:27:56 +0200

    firebase installed and added to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'brief-ionic-app',
+    appId: '1:665631817432:web:bcc77a01f1cabe10b4a1ad',
+    storageBucket: 'brief-ionic-app.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAGdts0Z_-NQCfa750Q6ukPsjMkCsvZaFY',
+    authDomain: 'brief-ionic-app.firebaseapp.com',
+    messagingSenderId: '665631817432',
+    measurementId: 'G-1XL7VNPDMH',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'brief-ionic-app',
+    appId: '1:665631817432:web:bcc77a01f1cabe10b4a1ad',
+    storageBucket: 'brief-ionic-app.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAGdts0Z_-NQCfa750Q6ukPsjMkCsvZaFY',
+    authDomain: 'brief-ionic-app.firebaseapp.com',
+    messagingSenderId: '665631817432',
+    measurementId: 'G-1XL7VNPDMH',
+  },
   production: false
 };
 
